5972bfde97a7e440f667638402f62d23a2ae15bd
darrny <darren.lim.off@gmail.com>
2024-11-24 11:50:28 +0800

    Updated for Mobile Viewing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Toggle mobile menu
+/* Toggle mobile menu */
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
